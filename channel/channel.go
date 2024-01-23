8911deae31ec57bd7adb4b06e6c779670dea49f9
ygqygq2 <ygqygq2@qq.com>
2024-01-23 15:58:59 +0800

    feat: channel 学习

---
+++ channel/channel.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func produce(ch chan<- int) {
+	for i := 0; i < 10; i++ {
+		ch <- i + 1
+		time.Sleep(time.Second)
+	}
+	close(ch)
+}
+
+func consume(ch <-chan int) {
+	for n := range ch {
+		println(n)
+	}
+}
+
+func main() {
+	ch := make(chan int, 5)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		produce(ch)
+		wg.Done()
+	}()
+
+	go func() {
+		consume(ch)
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
